e8df92cd32d6cfc6b9e21c5301658fec85096da5
Mateus Paiva <64433509+mampaiva@users.noreply.github.com>
2021-02-13 10:41:00 -0300

    Update avogadro-downgrade.sh

--- avogadro-downgrade.sh
+++ avogadro-downgrade.sh
@@ -4,7 +4,7 @@
 #use sudo or su to run this script
 
 echo "deb http://old-releases.ubuntu.com/ubuntu/ artful main restricted universe multiverse" >> /etc/apt/sources.list #add the ubuntu 17.10 repositories
-
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
 apt update 
 
 echo "Package:  *avogadro*  
